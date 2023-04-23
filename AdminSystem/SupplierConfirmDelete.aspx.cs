53001599f0a74b7726134c55c751e70e0dee911a
Sadiqul Nayeem <P2729234@my365.dmu.ac.uk>
2023-04-23 16:26:10 +0100

    Add, Update, Delete and Filter Method Completed.

--- AdminSystem/SupplierConfirmDelete.aspx.cs
+++ AdminSystem/SupplierConfirmDelete.aspx.cs
@@ -4,11 +4,26 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_ConfirmDelete : System.Web.UI.Page
 {
+    Int32 supplierNo;
     protected void Page_Load(object sender, EventArgs e)
     {
+        supplierNo = Convert.ToInt32(Session["supplierNo"]);
+    }
 
+    protected void btnYes_Click(object sender, EventArgs e)
+    {
+        clsSupplierCollection SupplierBook = new clsSupplierCollection();
+        SupplierBook.ThisSupplier.Find(supplierNo);
+        SupplierBook.Delete();
+        Response.Redirect("SupplierList.aspx");
+    }
+
+    protected void btnNo_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("SupplierList.aspx");
     }
 }
