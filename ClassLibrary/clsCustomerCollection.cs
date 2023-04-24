114e08a7c11aa858d748e61eb5b7d395dfa796c5
Mohammed Ahiam <mohammed.aniam99@gmail.com>
2023-04-24 16:43:43 +0100

    Done before presentation layer

--- ClassLibrary/clsCustomerCollection.cs
+++ ClassLibrary/clsCustomerCollection.cs
@@ -1,11 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace ClassLibrary
 {
     public class clsCustomerCollection
+
+
     {
         List<clsCustomer> mCustomerList = new List<clsCustomer>();
-       
+
+        public clsCustomerCollection()
+        {
+            clsCustomer TestItem = new clsCustomer();
+            TestItem.AccountActive = true;
+            TestItem.CustomerNo = 1;
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.FirstName = "aa";
+            TestItem.LastName = "aa";
+            TestItem.ContactNumber = "1234";
+            TestItem.Email = "sdsds@dsds";
+            mCustomerList.Add(TestItem);
+            TestItem = new clsCustomer();
+            TestItem.AccountActive = true;
+            TestItem.CustomerNo = 2;
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestItem.FirstName = "bb";
+            TestItem.LastName = "bb";
+            TestItem.ContactNumber = "2345";
+            TestItem.Email = "asdf@gfd";
+            mCustomerList.Add(TestItem);
+        }
 
         //private data member for the list
 
@@ -37,5 +61,27 @@ namespace ClassLibrary
         
         }
         public clsCustomer ThisCustomer { get; set; }
+        //public clsCustomerCollection()
+        //{
+        //    clsCustomer TestItem = new clsCustomer();
+        //    TestItem.AccountActive = true;
+        //    TestItem.CustomerNo = 1;
+        //    TestItem.DateAdded = DateTime.Now.Date;
+        //    TestItem.FirstName = "aa";
+        //    TestItem.LastName = "aa";
+        //    TestItem.ContactNumber = "1234";
+        //    TestItem.Email = "sdsds@dsds";
+        //    mCustomerList.Add(TestItem);
+        //    TestItem = new clsCustomer();
+        //    TestItem.AccountActive = true;
+        //    TestItem.CustomerNo = 2;
+        //    TestItem.DateAdded = DateTime.Now.Date;
+        //    TestItem.FirstName = "bb";
+        //    TestItem.LastName = "bb";
+        //    TestItem.ContactNumber = "2345";
+        //    TestItem.Email = "asdf@gfd";
+        //    mCustomerList.Add(TestItem);
+        //}
     }
+
 }
