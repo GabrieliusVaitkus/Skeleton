3243f5fef716bd5ad88528467ca3b589e69c218b
P2422047 <P2422047@my365.dmu.ac.uk>
2023-04-27 04:00:53 +0100

    Collection Class

---
+++ ClassLibrary/clsStockCollection.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ClassLibrary
+{
+    public class clsStockCollection
+    {
+       
+        public clsStockCollection()
+        {
+            Int32 Index = 0;
+            Int32 RecordCount = 0;
+            clsDataConnection DB = new clsDataConnection();
+            DB.Execute("sproc_tblStock_SelectAll");
+            RecordCount = DB.Count;
+            while (Index < RecordCount)
+            {
+
+                clsStock AnStock = new clsStock();
+                AnStock.CardNo = Convert.ToInt32(DB.DataTable.Rows[Index]["CardNo"]);
+                AnStock.CardDescription = Convert.ToString(DB.DataTable.Rows[Index]["CardDescription"]);
+                AnStock.CardType = Convert.ToString(DB.DataTable.Rows[Index]["CardType"]);
+                AnStock.Price = Convert.ToDecimal(DB.DataTable.Rows[Index]["Price"]);
+                AnStock.DateAdded = Convert.ToDateTime(DB.DataTable.Rows[Index]["DateAdded"]);
+                AnStock.Available = Convert.ToBoolean(DB.DataTable.Rows[Index]["Available"]);
+                mStockList.Add(AnStock);
+                Index++;
+            }
+        
+            
+        }
+        List<clsStock> mStockList = new List<clsStock>();
+        public List<clsStock> StockList
+        {
+            get
+            {
+                return mStockList;
+            }
+            set
+            {
+                mStockList = value;
+            }
+        }
+        public int Count
+        {
+            get
+            {
+                return mStockList.Count;
+            }
+            set
+            {
+
+            }
+        }
+        public clsStock ThisStock { get; set; }
+    }
+}
---
+++ Testing4/tstStockCollection.cs
@@ -0,0 +1,84 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Testing4
+{
+    [TestClass]
+    public class tstStockCollection
+    {
+        
+
+        [TestMethod]
+        public void InstanceOK()
+        {
+            //Create an instance of the class we want to create
+            clsStockCollection AllStocks = new clsStockCollection();
+            //test to see that it exists
+            Assert.IsNotNull(AllStocks);
+        }
+
+        [TestMethod]
+        public void StockListOK()
+        {
+            //Create an instance of the class we want to create
+            clsStockCollection AllStocks = new clsStockCollection();
+            List<clsStock> TestList = new List<clsStock>();
+            clsStock TestItem = new clsStock();
+            TestItem.Available = true;
+            TestItem.CardNo = 1;
+            TestItem.CardDescription = "Psychic";
+            TestItem.CardType = "Energy";
+            TestItem.Price = Convert.ToDecimal(9.100);
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestList.Add(TestItem);
+            AllStocks.StockList = TestList;
+            //test to see that it exists
+            Assert.AreEqual(AllStocks.StockList, TestList);
+        }
+
+       
+
+        [TestMethod]
+        public void ThisStockProprtyOK()
+        {
+            //Create an instance of the class we want to create
+            clsStockCollection AllStocks = new clsStockCollection();
+            clsStock TestStock = new clsStock();
+            TestStock.Available = true;
+            TestStock.CardNo = 1;
+            TestStock.CardDescription = "Psychic";
+            TestStock.CardType = "Energy";
+            TestStock.Price = Convert.ToDecimal(9.100);
+            TestStock.DateAdded = DateTime.Now.Date;
+            AllStocks.ThisStock = TestStock;
+            //test to see that it exists
+            Assert.AreEqual(AllStocks.ThisStock, TestStock);
+        }
+
+        [TestMethod]
+        public void ListAndCountOK()
+        {
+            //Create an instance of the class we want to create
+            clsStockCollection AllStocks = new clsStockCollection();
+            List<clsStock> TestList = new List<clsStock>();
+            clsStock TestItem = new clsStock();
+            TestItem.Available = true;
+            TestItem.CardNo = 1;
+            TestItem.CardDescription = "Psychic";
+            TestItem.CardType = "Energy";
+            TestItem.Price = Convert.ToDecimal(9.100);
+            TestItem.DateAdded = DateTime.Now.Date;
+            TestList.Add(TestItem);
+            AllStocks.StockList = TestList;
+            //test to see that it exists
+            Assert.AreEqual(AllStocks.Count, TestList.Count);
+        }
+
+        
+
+
+
+    }
+}
